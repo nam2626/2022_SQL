57a3baa6a19d2444a5a417622150ebb8d1c5b25c
NamSangKyu <nam2626@gmail.com>
2022-12-14 09:45:10 +0900

    INSERT

---
+++ 221214.sql
@@ -0,0 +1,13 @@
+--STUDENT 테이블에 데이터  5건 저장하는 INSERT문을 작성 후 실행
+insert into student(student_no,student_name,major_name,score)
+values('20221111','김철수','컴퓨터공학과',3.45);
+insert into student(student_no,student_name,major_name,score)
+values('20221112','이영희','경제학과',1.45);
+insert into student(student_no,student_name,major_name,score)
+values('20221113','박영수','경영학과',4.11);
+insert into student(student_no,student_name,major_name,score)
+values('20221114','장이성','국어국문학과',2.33);
+insert into student(student_no,student_name,major_name,score)
+values('20221115','김우형','생활체육학과',3.11);
+
+commit;
