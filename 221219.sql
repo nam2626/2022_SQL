96e0f78706ebf99ae7b1d80e92427427423a9ce4
nam2626 <nam2626@naver.com>
2022-12-19 11:27:18 +0900

    서브 쿼리

--- 221219.sql
+++ 221219.sql
@@ -38,7 +38,17 @@ where s.major_no = m.major_no(+) and s.score <= (select avg(score) from student)
 --장학금을 받는 학생들을 조회 (서브쿼리 이용해서 조회)
 select * from student where student_no in(select student_no from student_scholarship);
 
+--학생들중 장학금을 못받는 학생들의 정보를 조회
+--학번 이름 학과명 평점 조회
+select s.student_no, s.student_name, nvl(m.major_name,'학과코드오류'), s.score from student s, major m
+where s.major_no = m.major_no(+) and s.student_no not in(select student_no from student_scholarship);
 
-
-
+--학생들중 장학금을 못받는 학생들의 정보를 조회
+--학과별 인원수를 출력 --> 학과명, 인원수
+select major_name, count(*) from
+(select s.student_no, s.student_name, nvl(m.major_name,'학과코드오류') as major_name,
+s.score from student s, major m
+where s.major_no = m.major_no(+) and
+s.student_no not in(select student_no from student_scholarship))
+group by major_name;
 
