bd92af71f07f33b6db1fe81a8871ce1beae9d115
nam2626 <nam2626@naver.com>
2022-12-19 10:49:38 +0900

    서브 쿼리

---
+++ 221219.sql
@@ -0,0 +1,44 @@
+--학생정보 출력시 학번, 이름, 학과명, 평점
+select s.student_no, s.student_name, 
+(select m.major_name from major m where s.major_no = m.major_no),
+s.score
+from student s;
+
+select s.student_no, s.student_name, m.major_name, s.score 
+from student s ,major m WHERE s.major_no = m.major_no;
+
+--학생정보 출력시 학과 번호가 학과테이블에 없는 학생을 조회
+select * from student s
+where s.major_no not in(select m.major_no from major m);
+
+--학생정보 출력시 학번, 이름, 학과명, 평점, 학과명이 공학이 포함된 정보만 조회
+select * from
+(select s.student_no, s.student_name, m.major_name, s.score 
+from student s ,major m WHERE s.major_no = m.major_no)
+where major_name like '%공학%';
+
+--select 문을 이용해서 테이블을 작성
+--학생 정보 출력, 학번 이름, 학과명, 평점
+--평점이 전체 평균 이하인 학생들만 조회
+select s.student_no, s.student_name, nvl(m.major_name,'학과번호오류'), score 
+from student s, major m
+where s.major_no = m.major_no(+) and s.score <= (select avg(score) from student);
+--서브쿼리
+select s.student_no, s.student_name, 
+nvl((select major_name from major m where s.major_no = m.major_no),'학과번호오류') as major_name, s.score 
+from student s
+where s.score <= (select avg(score) from student);
+--테이블 작성시 select문으로 이용하는 방법
+create table low_student
+as 
+select s.student_no, s.student_name, nvl(m.major_name,'학과번호오류') as major_name, s.score 
+from student s, major m
+where s.major_no = m.major_no(+) and s.score <= (select avg(score) from student);
+
+--장학금을 받는 학생들을 조회 (서브쿼리 이용해서 조회)
+select * from student where student_no in(select student_no from student_scholarship);
+
+
+
+
+
